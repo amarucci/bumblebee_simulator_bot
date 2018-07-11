b9944472eb0be0508634336ed34e717f8f4a1ab8
Anthony Marucci <amarucci13@gmail.com>
2018-07-10 21:10:53 -0700

    change to lambdas in commands dictionary

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# spongebot
+# simulates this b-e-a-utiful dog called bumblebee
--- bumblebot.py
+++ bumblebot.py
@@ -1,5 +1,4 @@
 import telebot
-import re
 import random
 from telebot import types
 from secret_vars import TOKEN
@@ -11,25 +10,15 @@ f = open("bumblebee.barks")
 barks = [line.strip() for line in f.readlines()]
 f.close()
 
-
-def sit():
-    return "bumblebot sits" 
-
-def bark():
-    return random.choice(barks)
-
-def good_girl():
-    return "bumblebot wags her tail excitedly and runs around in a little circle"
+commands = {
+        "sit": lambda : "bumblebee sits",
+        "speak": lambda : random.choice(barks),
+        "good girl": lambda : "bumblebee wags her tail excitedly and runs around in a little circle",
+        "kill": lambda : kill 
+        }
 
 def kill():
-    return "bumblebot enters a blood frenzy. her eyes turn red and roll back into her head. she'll be back in an hour"
-
-commands = {
-        "sit": sit,
-        "speak": bark,
-        "good girl": good_girl,
-        "kill": kill
-        }
+    return "bumblebee enters a blood frenzy. her eyes turn red and roll back into her head. she'll be back in an hour"
 
 @bot.message_handler(func=lambda message: True)
 def respond_to_command(message):
@@ -38,7 +27,7 @@ def respond_to_command(message):
         for key, correct_func in commands.items():
             if key in message.text:
                 bot.reply_to(message, correct_func())
-    else: #bumblebot selects a random command
+    else: #bumblebee selects a random command
         bot.reply_to(message, random.choice(list(commands.values()))())
 
 bot.polling()
